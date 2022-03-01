4ec35e4abb50855bd9b0db60412453730a4c37a2
Hopertz <lugano.ngulwa@gmail.com>
2022-03-01 09:17:48 +0300

    added comments episode3

--- episode3/trig02.js
+++ episode3/trig02.js
@@ -1,3 +1,4 @@
+// Vertical oscillating motion
 window.onload = function(){
     var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
@@ -16,9 +17,9 @@ window.onload = function(){
         var y = centerY + Math.sin(angle)*offset
         context.clearRect(0,0,width,height);
         context.beginPath();
-        context.arc(centerX,y,50,0,Math.PI*2,false)
+        context.arc(centerX,y,50,0,Math.PI*2,false);
         context.fill();
-        angle += speed
+        angle += speed;
 
         requestAnimationFrame(render);
 
@@ -30,6 +31,43 @@ window.onload = function(){
 };
 
 /*
+// Horizontal oscillating motion
+window.onload = function(){
+    var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d'),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight;
+
+    var centerY = height * 0.5,
+        centerX = width * 0.5,
+        offset = height * 0.4,
+        speed = 0.05,
+        angle = 0;
+
+    render();
+
+    function render(){
+        var x = centerX + Math.sin(angle)*offset;
+        context.clearRect(0,0,width,height);
+        context.beginPath();
+        context.arc(x,centerY,50,0,Math.PI*2,false);
+        context.fill();
+        angle += speed;
+
+        requestAnimationFrame(render);
+
+        // CanvasPath.arc(x: number, y: number, radius: number, startAngle: number, endAngle: number, counterclockwise?: boolean): void
+    }
+
+       
+    
+};
+
+*/
+
+/*
+
+// Circle growing and shrinking
 window.onload = function(){
     var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
@@ -64,6 +102,8 @@ window.onload = function(){
 
 
 /*
+
+// Circle fading in and out smoothyly
 window.onload = function(){
     var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
