19ae1f4502c161442f2a9b23ef545c18bac79c34
Hopertz <lugano.ngulwa@gmail.com>
2022-03-03 07:30:07 +0300

    added episode7

---
+++ episode7/main.js
@@ -0,0 +1,10 @@
+var v1 =  vector.create(10,5),
+    v2 = vector.create(3,4);
+
+v1.addTo(v2);
+
+//console.log(v1.setAngle(Math.PI/6));
+//console.log(v1.setLength(100));
+console.log(v1.getX());
+console.log(v1.getY());
+
